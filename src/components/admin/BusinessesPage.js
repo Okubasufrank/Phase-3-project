5d6364427d4ee5b8c8f427da7dd67e2a1ff56fa0
Okubasufrank <franklineokubasu51@gmail.com>
2022-09-12 09:32:56 +0300

    Chose the options credentials change

--- src/components/admin/BusinessesPage.js
+++ src/components/admin/BusinessesPage.js
@@ -134,7 +134,7 @@ export default function BusinessesTable() {
             ) : null}
           </div>
           <div>
-            <label htmlFor="category-name">Select from below option</label>
+            <label htmlFor="category-name">choose from below options</label>
             <select
               name="category"
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
