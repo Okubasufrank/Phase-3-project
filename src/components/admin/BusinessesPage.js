9803d832c3864385e0f20ea0bd4869f06af49b0a
Okubasufrank <franklineokubasu51@gmail.com>
2022-09-12 10:04:47 +0300

    Services Offered

--- src/components/admin/BusinessesPage.js
+++ src/components/admin/BusinessesPage.js
@@ -189,7 +189,7 @@ export default function BusinessesTable() {
           </div>
           <div>
             <label htmlFor="services" className="sr-only">
-              Business services
+              Our services
             </label>
             <div className="flex">
               <input
