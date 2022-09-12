3a79bda04a995ecb4235d7da556261f06dfc488e
Okubasufrank <franklineokubasu51@gmail.com>
2022-09-12 10:18:02 +0300

    Adding the identification

--- src/components/admin/BusinessesPage.js
+++ src/components/admin/BusinessesPage.js
@@ -243,7 +243,7 @@ export default function BusinessesTable() {
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
-              ID
+              Identification
             </th>
             <th scope="col" className="px-6 py-3">
               Potrait
