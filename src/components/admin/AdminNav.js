e784f93b07f84a82ad8340aadbbe0e5e2a061d5b
Okubasufrank <franklineokubasu51@gmail.com>
2022-09-13 10:27:45 +0300

    main menu option

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -51,7 +51,7 @@ function AdminNavBar() {
               <div className="flex md:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
+                  <span className="sr-only">main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
